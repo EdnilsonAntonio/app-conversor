db933f533e04de8c821db0a69f0c9fe0135f5e6d
Ednilson António <ednilsonantonio1740@gmail.com>
2025-08-16 15:27:42 +0100

    app start

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native';
+import { Button } from './src/components/Button';
+import { styles } from './App.styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      {/* Tela principal scrollavel'*/}
+      <ScrollView>
+        <View style={styles.content}>
+          {/* Barra de status */}
+          <StatusBar style="light" />
+
+          {/* Cabeçalho */}
+          <View style={styles.header}>
+            <Text style={styles.title}>Conversor de moedas</Text>
+            <Text style={styles.subTitle}>Converta valores entre diferentes moedas</Text>
+          </View>
+
+          {/* Cartão de conversão */}
+          <View styles={styles.card}>
+            <Text styles={styles.label}>De:</Text>
+            <Button variant='secondary'></Button>
+          </View>
+
+        </View>
+      </ScrollView>
+
+    </KeyboardAvoidingView>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
